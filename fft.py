151c99234ce4620d788c06bce54cf0645239ff52
Tatok-n <tnhunter2004@gmail.com>
2025-11-19 19:40:28 -0500

    Initial file structure

---
+++ fft.py
@@ -0,0 +1,24 @@
+
+
+
+def main(mode : int) :
+    if mode == 1:
+        return fft()
+    elif mode == 2:
+        return denoise_image()
+    elif mode == 3:
+        return compress_image()
+    elif mode == 4:
+        return plot_runtime()
+
+def fft() :
+    pass
+
+def denoise_image() :
+    pass
+
+def compress_image() :
+    pass
+
+def plot_runtime() :
+    pass
